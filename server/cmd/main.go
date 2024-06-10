5c3588a9bd97c6c62016fe6849f76461c7c6cfae
Nitzan Papini <nitzanpap@gmail.com>
2024-06-11 00:34:47 +0300

    Added basic versioning

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -17,58 +17,73 @@ func setupRouter() *gin.Engine {
 	// gin.DisableConsoleColor()
 	r := gin.Default()
 
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 	})
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
+	v1 := r.Group("/v1")
+	{
 
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
+		v1.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"status": "ok", "version": "v1"})
+		})
 
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
+		// Ping test
+		v1.GET("/ping", func(c *gin.Context) {
+			c.String(http.StatusOK, "pong")
+		})
+		v1.GET("/health", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
+		})
 
-	return r
+		// Get user value
+		v1.GET("/user/:name", func(c *gin.Context) {
+			user := c.Params.ByName("name")
+			value, ok := db[user]
+			if ok {
+				c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+			} else {
+				c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+			}
+		})
+
+		// Authorized group (uses gin.BasicAuth() middleware)
+		// Same than:
+		// authorized := v1.Group("/")
+		// authorized.Use(gin.BasicAuth(gin.Credentials{
+		//	  "foo":  "bar",
+		//	  "manu": "123",
+		//}))
+		authorized := v1.Group("/", gin.BasicAuth(gin.Accounts{
+			"foo":  "bar", // user:foo password:bar
+			"manu": "123", // user:manu password:123
+		}))
+
+		/* example curl for /admin with basicauth header
+		   Zm9vOmJhcg== is base64("foo:bar")
+
+			curl -X POST \
+		  	http://localhost:8080/admin \
+		  	-H 'authorization: Basic Zm9vOmJhcg==' \
+		  	-H 'content-type: application/json' \
+		  	-d '{"value":"bar"}'
+		*/
+		authorized.POST("admin", func(c *gin.Context) {
+			user := c.MustGet(gin.AuthUserKey).(string)
+
+			// Parse JSON
+			var json struct {
+				Value string `json:"value" binding:"required"`
+			}
+
+			if c.Bind(&json) == nil {
+				db[user] = json.Value
+				c.JSON(http.StatusOK, gin.H{"status": "ok"})
+			}
+		})
+
+		return r
+	}
 }
 
 func main() {
