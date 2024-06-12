7c601a87c165603456d5d6f70dda028f05f7fa1b
Nitzan Papini <nitzanpap@gmail.com>
2024-06-12 17:05:25 +0300

    Added env to client side

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
+import { configurations } from "@/configs/config"
+import Image from "next/image"
 
 export default function Home() {
+  console.log(configurations)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -109,5 +112,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  );
+  )
 }
---
+++ client/src/configs/config.ts
@@ -0,0 +1,6 @@
+export const configurations = {
+  envVars: {
+    environment: process.env.NEXT_PUBLIC_ENV,
+    serverBaseUrl: process.env.NEXT_PUBLIC_SERVER_BASE_URL,
+  },
+}
