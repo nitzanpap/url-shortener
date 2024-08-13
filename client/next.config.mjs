b17d89e427c856a567111433c4e071b61326cfd7
nitzanpap <nitzanpap@gmail.com>
2024-08-14 00:52:29 +0300

    Add PWA

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,13 +1,22 @@
 // Purpose: Next.js configuration file.
 import path from "path"
-
-// @ts-check
+import withPWA from "next-pwa"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
   sassOptions: {
     includePaths: [path.join(new URL(".", import.meta.url).pathname, "styles")],
   },
 }
 
-export default nextConfig
+export default withPWA({
+  dest: "public", // destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
+  register: true, // register the PWA service worker
+  skipWaiting: true, // skip waiting for service worker activation
+})(nextConfig)
